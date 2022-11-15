5d0d6d2e771af7413e7544fd9b075101842afc10
athena272 <guilhermera272@gmail.com>
2022-11-14 22:54:04 -0300

    Update search.service.ts

--- src/app/services/search/search.service.ts
+++ src/app/services/search/search.service.ts
@@ -9,7 +9,7 @@ export class SearchService {
   constructor(private http: HttpClient) {}
 
   search(query: string) {
-    const apiUrl = `https://api.spoonacular.com/recipes/complexSearch?apiKey=5bc2abb6dd144f41aa93da15d256aa03&query=${query}&number=21`;
+    const apiUrl = `https://api.spoonacular.com/recipes/complexSearch?apiKey=c0ae654b6a3445cf8b4e0fea42008718&query=${query}&number=12`;
 
     return this.http.get(apiUrl);
   }
